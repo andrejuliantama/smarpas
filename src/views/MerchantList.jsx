b8594d6f1bbec720fdbdcc2c8b3a2139295732bb
Andre Juliantama <juliantamaandre@gmail.com>
2020-04-23 04:43:58 +0700

    udah bisa pake modal

--- src/views/MerchantList.jsx
+++ src/views/MerchantList.jsx
@@ -17,15 +17,43 @@
 */
 import React, { Component } from "react";
 import { Grid, Row, Col, Table } from "react-bootstrap";
-
+import StatsCard from "components/StatsCard/StatsCard";
 import Card from "components/Card/Card.jsx";
 import { thMerchant, tdMerchant } from "variables/Variables.jsx";
+
+
 
 class TableList extends Component {
   render() {
     return (
       <div className="content">
         <Grid fluid>
+        <p className="copyright pull-left">
+          <a
+            href="#"
+            target="_blank"
+            className="btn btn-info btn-fill"
+          >
+            Add Merchant
+          </a>
+          
+          <a
+            href="#"
+            target="_blank"
+            className="btn btn-danger btn-fill btn-info"
+          >
+            Delete Merchant
+          </a>
+        </p>
+        <p className="copyright pull-right">
+          <a
+            href="#"
+            target="_blank"
+            className="btn btn-success btn-fill"
+          >
+            Export CSV
+          </a>
+        </p>
           <Row>
             <Col md={12}>
               <Card
@@ -58,37 +86,6 @@ class TableList extends Component {
               />
             </Col>
 
-            <Col md={12}>
-              <Card
-                plain
-                title="Striped Table with Hover"
-                category="Here is a subtitle for this table"
-                ctTableFullWidth
-                ctTableResponsive
-                content={
-                  <Table hover>
-                    <thead>
-                      <tr>
-                        {thMerchant.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
-                        })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {tdMerchant.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
-                            })}
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
-                }
-              />
-            </Col>
           </Row>
         </Grid>
       </div>
--- src/views/UserList.jsx
+++ src/views/UserList.jsx
@@ -16,57 +16,103 @@
 
 */
 import React, { Component } from "react";
-import { Grid, Row, Col, Table } from "react-bootstrap";
-
+import { Grid, Row, Col, Table, Button, Modal } from "react-bootstrap";
 import Card from "components/Card/Card.jsx";
 import { thUser, tdUser } from "variables/Variables.jsx";
+import StatsCard from "components/StatsCard/StatsCard";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
 
-class TableList extends Component {
+class UserList extends Component {
+  state = { show: false };
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
   render() {
     return (
       <div className="content">
         <Grid fluid>
+          <p className="copyright pull-left">
+            <button
+              href="#"
+              target="_blank"
+              className="btn btn-info btn-fill"
+            >
+              Add User
+            </button>
+            
+            <a
+              href="#"
+              target="_blank"
+              className="btn btn-danger btn-fill"
+            >
+              Delete User
+            </a>
+          </p>
+          <p className="copyright pull-right">
+            <Button
+              href=""
+              target="_blank"
+              className="btn btn-success btn-fill"
+              onClick={this.showModal}
+            >
+              Top Up
+            </Button>
+            <Modal show={this.state.show}>
+              <Modal.Header>
+                <Modal.Title>Top Up</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <FormInputs
+                  ncols = {["col-md-5" , "col-md-3" , "col-md-4"]}
+                  properties = {[
+                      {
+                          label : "NIM",
+                          type : "number",
+                          bsClass : "form-control",
+                          placeholder : "Nim",
+                      },
+                      {
+                          label : "Nominal",
+                          type : "number",
+                          bsClass : "form-control",
+                          placeholder : "Nominal"
+                      },
+                      {
+                          label : "PIN",
+                          type : "password",
+                          bsClass : "form-control",
+                          placeholder : "PIN"
+                      }
+                  ]}
+                />
+              </Modal.Body>
+              <Modal.Footer>
+                <Button className="btn btn-danger btn-fill" onClick={this.hideModal}>
+                  Cancel
+                </Button>
+                <Button className="btn btn-info btn-fill" type="submit" onClick={event =>  window.location.href=''}>
+                  Top Up
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </p>
+          
           <Row>
             <Col md={12}>
               <Card
                 title="User List"
                 category="Institut Teknologi Bandung"
+                
                 ctTableFullWidth
                 ctTableResponsive
                 content={
                   <Table striped hover>
-                    <thead>
-                      <tr>
-                        {thUser.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
-                        })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {tdUser.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
-                            })}
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
-                }
-              />
-            </Col>
-
-            <Col md={12}>
-              <Card
-                plain
-                title="Striped Table with Hover"
-                category="Here is a subtitle for this table"
-                ctTableFullWidth
-                ctTableResponsive
-                content={
-                  <Table hover>
                     <thead>
                       <tr>
                         {thUser.map((prop, key) => {
@@ -96,4 +142,4 @@ class TableList extends Component {
   }
 }
 
-export default TableList;
+export default UserList;
